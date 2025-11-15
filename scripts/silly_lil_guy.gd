0547812ef1e8ba2eadee82bfc676afde5af52fe2
fifi <gayottergal@gmail.com>
2025-11-15 02:59:54 -0500

    commit test stuff for patrol route

---
+++ scripts/silly_lil_guy.gd
@@ -0,0 +1,16 @@
+extends CharacterBody3D
+
+@onready var patrol_route: PatrolRoute = $PatrolRoute
+
+func _ready() -> void:
+	pass 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		patrol_route.target_next_point()
+
+func _physics_process(_delta: float) -> void:
+	var direction = patrol_route.direction_to_next_point()
+	
+	velocity = direction * 5.0
+	move_and_slide()
