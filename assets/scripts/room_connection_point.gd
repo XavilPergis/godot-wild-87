6ba8174ef984867136f5c174b1a475ea7cdc3db2
fifi <gayottergal@gmail.com>
2025-11-15 21:06:17 -0500

    refactor room positioning logic into RoomConnectionPoint class

--- assets/scripts/room_connection_point.gd
+++ assets/scripts/room_connection_point.gd
@@ -2,3 +2,12 @@ extends Node3D
 class_name RoomConnectionPoint
 
 var connection: RoomConnectionPoint = null
+
+func connect_and_position(p_connection: RoomConnectionPoint):
+	p_connection.connection = self
+	connection = p_connection
+	
+	var rotated_transform = Transform3D(self.global_transform)
+	rotated_transform.basis = rotated_transform.basis.rotated(Vector3(0, 1, 0), PI)
+	self.get_parent_node_3d().global_transform *= p_connection.global_transform * rotated_transform.inverse()
+	pass
--- assets/scripts/room_connection_test_scene.gd
+++ assets/scripts/room_connection_test_scene.gd
@@ -20,23 +20,18 @@ func expand_dungeon_recursive(start_room: Node3D, rooms_left: int) -> int:
 	var conn_points = get_connection_points(start_room)
 	var rooms_added = 0
 
-				
 	for conn_point in conn_points:
 		if rooms_added >= rooms_left:
 					break
-					
+		
 		if not conn_point.connection:
 			var new_room = _rooms_unpacked[randi_range(0, _rooms_unpacked.size() - 1)].duplicate()
 			add_child(new_room)
 			
 			var new_conn_points: = get_connection_points(new_room)
 			var new_conn_point = new_conn_points[randi_range(0, new_conn_points.size() - 1)]
-			conn_point.connection = new_conn_point
-			new_conn_point.connection = conn_point
 			
-			var target_transform = Transform3D(new_conn_point.global_transform)
-			target_transform.basis = target_transform.basis.rotated(Vector3(0, 1, 0), PI)
-			new_room.global_transform *= conn_point.global_transform * target_transform.inverse()
+			new_conn_point.connect_and_position(conn_point)
 			
 			rooms_added = rooms_added + 1
 			
